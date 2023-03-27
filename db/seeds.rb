04a63206950b8ca0c9dea73ee7d18d9e54a5f236
tony555t <tonytullah764@gmail.com>
2023-03-27 16:41:23 +0300

    seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# seed.rb
+
+# Create some heroes
+hero1 = Hero.create(name: "Bruce Wayne", super_name: "Batman")
+hero2 = Hero.create(name: "Clark Kent", super_name: "Superman")
+
+# Create some powers
+power1 = Power.create(name: "Super strength", description: "The ability to lift heavy objects")
+power2 = Power.create(name: "Flight", description: "The ability to fly")
+
+# Assign powers to heroes
+HeroPower.create(hero: hero1, power: power1, strength: "10")
+HeroPower.create(hero: hero1, power: power2, strength: "7")
+HeroPower.create(hero: hero2, power: power1, strength: "9")
+
+puts "Seeding complete."
