1e7206a3a28bb5fd1fcbfc12b8b5e7734dd5aaf8
tony555t <tonytullah764@gmail.com>
2023-03-27 18:20:38 +0300

    hero validation

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,7 +1,22 @@
 class HeroesController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     def index
         heroes=Hero.all
-        render json: heroes,stataus: :ok
+        render json: heroes.to_json(except:[:created_at, :updated_at])
     end
+
+    def show
+        hero = find_hero
+        render json: hero.to_json(only: [:id, :name, :super_name])
+    end
+  private
+   def find_hero
+    Hero.find_by(params[:id])
+   end
+
+   def render_not_found_response
+    render json: {error: 'Hero Not Found'}, status: :render_not_found_response
+   end
+
+
 end
