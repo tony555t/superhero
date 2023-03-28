be727ad649308525908daf9b2b8c3281f844c557
tony555t <tonytullah764@gmail.com>
2023-03-28 21:51:44 +0300

    nested api

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -4,15 +4,14 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
         heroes=Hero.all
         render json: heroes.to_json(except:[:created_at, :updated_at])
     end
-
-    def show 
-        hero = Hero.find_by(id: params[:id])
-        if hero 
-            render json: hero, status: :ok
-        else 
-            render json: { error: "Hero not found" }, status: :not_found
+    def show
+        hero = Hero.includes(:powers).find_by(id: params[:id])
+        if hero.present?
+          render json: hero.as_json(include: { powers: { only: [:id, :name, :description] } })
+        else
+          render json: { error: 'Hero not found' }, status: :not_found
         end
-    end
+      end
   
 
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -3,6 +3,13 @@ class PowersController < ApplicationController
         powers =  Power.all
         render json: powers, only: [:id, :name, :description]
     end
-    
+    def show 
+        power = Power.find_by(id: params[:id])
+        if power
+            render json: power
+            else
+                render json:{error:"power not"},status: :not_found
+            end
+        end
 
 end
